daf574a3cc9d7a1662498a473d27fe551dc0ce65
morrdrix <99170437+morrdrix@users.noreply.github.com>
2025-08-07 15:59:18 +0200

    Update logprint.py
    Added writing Timestamps
    Temporary Configuration

--- logprint/logprint/logprint.py
+++ logprint/logprint/logprint.py
@@ -11,22 +11,38 @@
 # Copyright © 2020 Julian Klein
 #
 
-import builtins as __builtin__
 
-__version__ = '0.1.0'
+import builtins as __builtin__
+import datetime
+from sys import argv
+
+__version__ = '1.1.0'
+
+_MAIN_SCRIPT = argv[0]
+
 
 class LogPrint:
     """
-LogPrint Class
+    LogPrint Class
 
-Description:
-This class is used to override the built-in print function and log the output to a file and screen.
+    Description:
+    This class is used to override the built-in print function and log the output to a file and screen.
+    Using this logger existing code can be augumented to logging functionality easily.
 
-:methods
-__init__(self, file_name='log.txt', fw_mode='w')
-print(self, *args, mode='both', **kwargs) - Overrides the built-in print function and logs the output to a file.
+    Usage info:
+    - implemented as Singelton (only one instance of LogPrint allowed)
+    - output configuration can be set with set_config(**kwargs)
+    - default output configuration:
+        timestamp_format: '[%Y-%m-%d %H:%M:%S]' (string)
+        timestamp: False (bool)
+        screen: True (bool)
+        file: True (bool)
 
-"""
+    - output configuration can be overridden temporarily by passing configuration **kwargs to print()
+    - output streams to screen and file are always identical when both are activated
+    - print() keywords "file" and "flush" are used by LogPrint.print( *args, **kwargs) and get overwitten if set
+    """
+
     _ninst = 0  # instance counter
 
     def __new__(cls, *args, **kwargs):
@@ -35,25 +51,36 @@ print(self, *args, mode='both', **kwargs) - Overrides the built-in print functio
         :return Instance or None
         '''
         if LogPrint._ninst:  # already an instance running
-            __builtin__.print(f'Error: only one instance of {LogPrint.__name__} allowed')
+            print(f'Error: only one instance of {cls.__name__} allowed')
             return None
         else:
             return super().__new__(cls)
 
-    def __init__(self, file_name='log.txt', fw_mode='w'):
+    def __init__(self, file='log.txt', write_mode='w', print_header='False'):
         '''
         Initializes the class and opens the log file.
-        :param file_name: File name (default 'log.txt')
-        :param fw_mode: Write mode (default 'w')
+        :param file: File name (default 'log.txt')
+        :param write_mode: Write mode (default: 'w' from ['w','a'])
+        :param print_header: print logging informaton at start
         '''
         LogPrint._ninst += 1  # register instance
+
+        self.script_name = _MAIN_SCRIPT
         self.file_hnd = None  # file handle
-        self.fw_mode = fw_mode  # write mode
-        self.file_name = file_name  # file name
+        self.file_name = file
+
+        self.configuration = {'timestamp_format': '[%Y-%m-%d %H:%M:%S]',
+                              'timestamp': False,
+                              'screen': True,
+                              'file': True,
+                              }
         try:
-            self.file_hnd = open(self.file_name, fw_mode)
+            self.file_hnd = open(self.file_name, write_mode, encoding='utf-8')
         except IOError as e:
             print(e)
+
+        if print_header:
+            self.print(f'logging {self.script_name} to {self.file_name}')
 
     def __del__(self):
         '''
@@ -67,32 +94,72 @@ print(self, *args, mode='both', **kwargs) - Overrides the built-in print functio
             print(e)
             return False
 
-    # funtion to override standart print function
-    def print(self, *args, mode='both', **kwargs):
+    def set_temp_config(self, **kwargs):
+        '''
+        create manipulated copy of configuration dict
+        manipulation is defined by **kwargs
+        :param kwargs:
+        :return: configuration dictionary
+        '''
+        temporary_configuration = self.configuration.copy()
+        excess_kwargs = dict()
+
+        for key in kwargs:
+            if key in temporary_configuration:
+                temporary_configuration[key] = kwargs[key]
+            else:
+                excess_kwargs[key] = kwargs[key]
+
+        return temporary_configuration, excess_kwargs
+
+    def set_config(self, **kwargs):
+        '''
+        set specified keys in configuration dict
+        :param kwargs:
+        :return: None
+        '''
+        self.configuration = self.set_temp_config(**kwargs)[0]
+
+    def print(self, *args, **kwargs):
         '''
         Method to override standart print() with
-        :param args: see print() 
-        :param mode: output target ['screen','file','both'] (default 'both')
-        :param kwargs: see print() 
+        Output is handled according to configuration
+        Output handling is overridden by inline configuration kwargs
+        :param args: see print()
+        :param kwargs: see print()
         '''
-        if mode == 'both' or mode == 'screen':
-            __builtin__.print(*args, **kwargs)  # screen output
-        if mode == 'both' or mode == 'file':
-            kwargs['file'] = self.file_hnd
-            kwargs['flush'] = True  # for instant writing
-            __builtin__.print(*args, **kwargs)  # file output
+        temp_config, print_kwargs = self.set_temp_config(**kwargs)
+
+        if temp_config['timestamp']:
+            args = [datetime.datetime.now().strftime(temp_config['timestamp_format'])] + list(args)
+
+        if temp_config['screen']:
+            __builtin__.print(*args, **print_kwargs)  # screen output
+
+        if temp_config['file']:
+            print_kwargs['file'] = self.file_hnd
+            print_kwargs['flush'] = True  # for instant writing
+            __builtin__.print(*args, **print_kwargs)  # file output
 
 
 if __name__ == '__main__':
     ''' Usage 
     '''
-    log_file = 'log.txt'
-    log = LogPrint(file_name=log_file)  # create Logprint instance
+
+    log = LogPrint()  # create Logprint instance
+
     print = log.print  # override buildin print()
 
-    print(f'This text should appear on screen and in log-file {log_file}', 99, sep=' <sep> ', end='<end>\n')
-    print(f'This text should appear on screen only', 99, sep=' <sep> ', end='<end>\n', mode='screen')
-    print(f'This text should appear on log-file only', 99, sep=' <sep> ', end='<end>\n', mode='file')
+    # configure output
+    log.set_config(timestamp=True)
+    print(f'This timestamp and text should appear on screen and in log-file {log.file_name}', sep=' <sep> ',
+          end='<end>\n')
+    # configure output temporarily with inline configuration kwargs
+    print(f'This text should appear on screen and in log-file {log.file_name}', 'a second positional argument',
+          sep=' <sep> ', end='<end>\n', timestamp=False)
+    # previous inline outpot configuration was temporary
+    print(f'This text and timestamp should appear on screen and in log-file {log.file_name} again',
+          'a second positional argument and a third positional integer argument', 99, sep=' <sep> ', end='<end>\n')
 
     # creating a second instance fails
-    log1 = LogPrint(file_name=log_file)
+    log1 = LogPrint()
